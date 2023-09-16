a64432151628347ff3351bc6ce74152c2d03aca8
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-09-16 22:44:40 +0700

    readTxt File such as SPL

---
+++ bin/algeo/Main.java
@@ -0,0 +1,9 @@
+package algeo;
+
+public class Main {
+    public static void main(String[] args){
+        MatrixType spl = new MatrixType();
+        spl.inputSPLText();
+        spl.printAugmentedMatrix();
+    }
+}
