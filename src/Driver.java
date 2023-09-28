ad9522a7ebb3e78c537ebaac7e758d09c3e0affa
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-09-28 16:57:06 +0700

    Image Proccess

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,27 @@
 //import operations.OBE;
 import models.*;
+import operations.*;
 
 public class Driver {
     public static void main(String[] args) {
-        ImageBSI.setDMat();
-        DeterminanInvers det = new DeterminanInvers(16,16);
-        det.inputMatriksFile("src/operations/D.txt");
-        ImageBSI.DMat.displayMatrix();
-        boolean adaBeda = false;
-        for (int i = 0; i < 16; i++){
-            for (int j = 0; j < 16; j++){
-                if (ImageBSI.DMat.getElmt(i, j) != det.contents.getMElmt(i, j)){
-                    adaBeda = true;
-                    System.out.println(i+1 + " " + (j+1));
-                }
-            }
-        }
-        System.out.println(adaBeda);
+        //ImageBSI.setDMat();
+        //DeterminanInvers det = new DeterminanInvers(16,16);
+        //det.inputMatriksFile("src/operations/D.txt");
+        //ImageBSI.DMat.displayMatrix();
+        //boolean adaBeda = false;
+        //for (int i = 0; i < 16; i++){
+        //    for (int j = 0; j < 16; j++){
+        //        if (ImageBSI.DMat.getElmt(i, j) != det.contents.getMElmt(i, j)){
+        //            adaBeda = true;
+        //            System.out.println(i+1 + " " + (j+1));
+        //        }
+        //    }
+        //}
+        //System.out.println(adaBeda);
+        ImageBSI.setXInvxDMat();
+        Matrix temp = new Matrix(ImageBSI.XInvxDMat);
+        temp.multiplyByConst(16);
+        temp.displayMatrix();
+
     }
 }
--- src/models/ImageBSI.java
+++ src/models/ImageBSI.java
@@ -4,6 +4,23 @@ import operations.Matrix;
 
 public class ImageBSI{
     public static Matrix DMat = new Matrix(16,16);
+    //public static Matrix XInv = new Matrix(16, 16);
+    public static Matrix XInvxDMat = new Matrix(16, 16);
+    public double[] aValue = new double[16];
+    public String sPath;
+    public String dPath;
+    //if(XInv == null){
+    //    BicubicSpline.setStaticInvX();
+    //    XInv = new Matrix(BicubicSpline.invX);
+    //}
+    public ImageBSI(){
+        this("","");
+    }
+
+    public ImageBSI(String sPath, String dPath){
+        this.sPath = sPath;
+        this.dPath = dPath;
+    }
 
     public static void setDMat(){
         double[][] var = new double[][]{{0,0},{1,0},{0,1},{1,1}};
@@ -17,29 +34,29 @@ public class ImageBSI{
                         row = x + (int)(arr[0] + arr[1]*2);
                         if (x == 0){
                             if (arr[0] == i && arr[1] == j){
-                                DMat.setElmt(4, row, idx);
+                                DMat.setElmt(1, row, idx);
                             }
                         } else if (x == 4){
                             if (arr[0]+1 == i && arr[1] == j){
-                                DMat.setElmt(2, row, idx);
+                                DMat.setElmt(0.5, row, idx);
                             } else if (arr[0]-1 == i && arr[1] == j){
-                                DMat.setElmt(-2, row, idx);
+                                DMat.setElmt(-0.5, row, idx);
                             }
                         } else if (x == 8){
                             if (arr[0] == i && arr[1] + 1 == j){
-                                DMat.setElmt(2, row, idx);
+                                DMat.setElmt(0.5, row, idx);
                             } else if (arr[0] == i && arr[1] - 1 == j){
-                                DMat.setElmt(-2, row, idx);
+                                DMat.setElmt(-0.5, row, idx);
                             }
                         } else {
                             if (arr[0]+1 == i && arr[1]+1 == j){
-                                DMat.setElmt(1, row, idx);
+                                DMat.setElmt(0.25, row, idx);
                             } else if (arr[0] == i && arr[1] - 1 == j){
-                                DMat.setElmt(-1, row, idx);
+                                DMat.setElmt(-0.25, row, idx);
                             } else if (arr[0]-1 == i && arr[1] == j){
-                                DMat.setElmt(-1, row, idx);
+                                DMat.setElmt(-0.25, row, idx);
                             } else if (arr[0] == i && arr[1] == j){
-                                DMat.setElmt(1, row, idx);
+                                DMat.setElmt(0.25, row, idx);
                             }
                         }
                         idx++;
@@ -47,5 +64,13 @@ public class ImageBSI{
                 }
             }
         }
+    }
+
+    public static void setXInvxDMat(){
+        setDMat();
+        if (BicubicSpline.invX == null){
+            BicubicSpline.setStaticInvX();
+        }
+        XInvxDMat = new Matrix(BicubicSpline.invX.multiplyMatrix(DMat));
     }
 }
