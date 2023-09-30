6f1b3ec9ebea02a97e22e356143d5794c6fad14f
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-09-30 09:07:55 +0700

    Testing Scale Image aja mas

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,11 @@ import operations.*;
 
 public class Driver {
     public static void main(String[] args) {
-        ImageBSI temp = new ImageBSI("src/evaa.jpeg", "test/evababy.png");
+        ImageBSI temp = new ImageBSI("src/monachan.png", "test/monachan.png");
         //BicubicSpline.setStaticInvX();
         //BicubicSpline.invX.displayMatrix();
         ImageBSI.setXInvxDMat();
         //ImageBSI.XInvxDMat.displayMatrix();
-        temp.scaleImage(5);
+        temp.scaleImage(7.89);
     }
 }
