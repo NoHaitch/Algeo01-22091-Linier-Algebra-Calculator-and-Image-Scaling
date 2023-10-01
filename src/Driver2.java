ea6b54f531b9c35a466972f1f8de2ca7f757b069
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-10-01 16:52:20 +0700

    asdasdasdasd

--- src/Driver2.java
+++ src/Driver2.java
@@ -1,16 +1,9 @@
 import models.*;
+import operations.*;
 
 public class Driver2 {
     public static void main(String[] args) {
-        DeterminanInvers temp = new DeterminanInvers();
-        temp.inputMatriksFile("src/inputSecond.txt");
-        temp.CalculateOBE();
-        System.out.println(temp.contents.getStep());
-        for (int i = 0; i < temp.countMul; i++){
-            System.out.println(temp.multiply[i]);
-        }
-        System.out.println("\n");
-        System.out.println(temp.sign);
-        System.out.println(temp.result);
+        Matrix temp = new Matrix();
+        temp.getElmt(0, 0);
     }
 }
